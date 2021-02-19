10a9132c80d731c5cba5fb32ba63c78da2a631c2
tong.luo <tong.luo@example.com>
2021-02-19 15:20:00 +0800

    “add 

---
+++ exam-test/src/main/java/com/exam/examtest/ExamTestApplication.java
@@ -0,0 +1,13 @@
+package com.exam.examtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamTestApplication.class, args);
+	}
+
+}
