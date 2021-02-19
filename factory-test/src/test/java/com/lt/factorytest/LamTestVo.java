5f15794a4170c43a8104e690fa3045587169e0c2
tong.luo <tong.luo@example.com>
2021-02-19 10:40:18 +0800

    add edit files of daily

---
+++ factory-test/src/test/java/com/lt/factorytest/LamTestVo.java
@@ -0,0 +1,32 @@
+package com.lt.factorytest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author tong.luo
+ * @description LamTestVo
+ * @date 2021/2/19 10:24
+ */
+//@ToString
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Data
+public class LamTestVo implements Serializable {
+    private static final long serialVersionUID = -2285645856392195673L;
+
+//    @Getter
+//    @Setter
+    private int agel;
+
+//    @Getter
+//    @Setter
+    private String name ;
+
+}
