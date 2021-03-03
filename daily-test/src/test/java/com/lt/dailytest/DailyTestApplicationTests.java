48128355122a20e29c973fefcd7ed3099fa2f9d1
tong.luo <luotong@dazhangfang.com>
2021-03-03 11:07:27 +0800

    fix commit files

---
+++ daily-test/src/test/java/com/lt/dailytest/DailyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.dailytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
