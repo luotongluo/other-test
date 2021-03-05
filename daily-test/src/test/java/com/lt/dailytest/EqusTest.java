9f865512fa798dfca1493a2edd72f11687198b9c
tong.luo <luotong@dazhangfang.com>
2021-03-05 13:45:30 +0800

    fix files

---
+++ daily-test/src/test/java/com/lt/dailytest/EqusTest.java
@@ -0,0 +1,14 @@
+package com.lt.dailytest;
+
+/**
+ * @author tong.luo
+ * @description EqusTest
+ * @date 2021/3/5 10:45
+ */
+public class EqusTest {
+    public static void main(String[] args) {
+        String equals = "PASS";
+        boolean equals1 = equals.equals(null);
+        System.out.println(equals1);
+    }
+}
