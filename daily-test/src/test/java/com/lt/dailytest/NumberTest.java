25b28392a519e56abd2a99022bdd83b58a5456d4
tong.luo <luotong@dazhangfang.com>
2021-03-10 11:01:59 +0800

    fix pages & add test class

---
+++ daily-test/src/test/java/com/lt/dailytest/NumberTest.java
@@ -0,0 +1,25 @@
+package com.lt.dailytest;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+/**
+ * @author tong.luo
+ * @description NumberTest
+ * @date 2021/3/10 10:45
+ */
+public class NumberTest {
+    public static void main(String[] args) {
+        double a = 2000.1234;
+        String str = NumberFormat.getIntegerInstance(Locale.getDefault()).format(a);     //转为千分符字符串
+        System.out.println(str);
+
+        try {
+            int b = NumberFormat.getIntegerInstance(Locale.getDefault()).parse(str).intValue();    //转为数字
+            System.out.println("" + b);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
