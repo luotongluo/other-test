f2ef7b65459dc009a201b23465b6b203533d9ecd
tong.luo <luotong@dazhangfang.com>
2021-03-25 11:14:45 +0800

    add

--- daily-test/src/main/java/com/lt/dailytest/tree/InterfaceMap.java
+++ daily-test/src/main/java/com/lt/dailytest/tree/InterfaceMap.java
@@ -8,24 +8,24 @@ import javax.swing.tree.TreeNode;
  * @date 2021/3/22 14:38
  */
 public interface InterfaceMap {
-    void put(K key, V value);
-
-    V get(K key);
-
-    void delete(K key);
-
-    int size();
-
-    Iterable<K> keys();
-
-    Iterable<TreeNode> nodes();
-
-    default boolean contains(K key) {
-        return get(key) != null;
-    }
-
-    default boolean isEmpty() {
-        return size() == 0;
-    }
+//    void put(K key, V value);
+//
+//    V get(K key);
+//
+//    void delete(K key);
+//
+//    int size();
+//
+//    Iterable<K> keys();
+//
+//    Iterable<TreeNode> nodes();
+//
+//    default boolean contains(K key) {
+//        return get(key) != null;
+//    }
+//
+//    default boolean isEmpty() {
+//        return size() == 0;
+//    }
 
 }
