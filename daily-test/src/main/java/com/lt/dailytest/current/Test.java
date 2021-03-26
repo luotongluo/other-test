d05a40707be45dbc558af93954f8b359736c4fb2
tong.luo <luotong@dazhangfang.com>
2021-03-26 15:25:05 +0800

    add fils

--- daily-test/src/main/java/com/lt/dailytest/current/Test.java
+++ daily-test/src/main/java/com/lt/dailytest/current/Test.java
@@ -13,5 +13,8 @@ public class Test {
         long l1 = Runtime.getRuntime().maxMemory();
         long l2 = Runtime.getRuntime().totalMemory();
         System.out.println("totalMemory:" + l2 / 1000D + " \t maxMemory:" + l1 / 1000D + " \t freeMemory:" + l / 1000D);
+
+        String name = Test.class.getName();
+        System.out.println(name);
     }
 }
